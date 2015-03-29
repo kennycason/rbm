ce2831e9b2a5559bdd9fcfcc20d39121c0dda6e6
Kenny Cason <kenneth.cason@gmail.com>
2015-03-29 13:13:55 -0500

    rbm bp test updates

--- src/test/java/nn/rbm/learn/TestBackErrorPropagation.java
+++ src/test/java/nn/rbm/learn/TestBackErrorPropagation.java
@@ -21,31 +21,94 @@ public class TestBackErrorPropagation {
 
     private static final DoubleFunction ROUND = new Round();
 
-    /**
-     * given the structure of an rbm, this will not learn the same way a layered bep nn will,
-     * but should still at least be able to propagate errors back
-     */
     @Test
-    public void backErrorPropagation() {
-
-        // even/odd number of 1s
+    public void backErrorPropagationOr() {
+        // or
         final List<Matrix> trainData = new ArrayList<>();
-        trainData.add(DenseMatrix.make(new double[][]{{0, 0, 1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,1}}));
+
+        final List<Matrix> teacherSignals = new ArrayList<>();
+        teacherSignals.add(DenseMatrix.make(new double[][] {{0}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+
+        final RBM rbm = new RandomRBMFactory().build(2, 1);
+
+        final LearningParameters learningParameters = new LearningParameters().setEpochs(10000).setLearningRate(0.1);
+        final BackErrorPropagation backErrorPropagation = new BackErrorPropagation(learningParameters);
+
+        LOGGER.info(rbm.getWeights());
+        backErrorPropagation.learn(rbm, trainData, teacherSignals);
+        LOGGER.info(rbm.getWeights());
+        for(Matrix input : trainData) {
+            final Matrix output = backErrorPropagation.feedFoward(rbm, input);
+            LOGGER.info(input + " -> " + output.apply(ROUND));
+        }
+    }
+
+    @Test
+    public void backErrorPropagationOr2() {
+        // or
+        final List<Matrix> trainData = new ArrayList<>();
+        trainData.add(DenseMatrix.make(new double[][] {{0,0,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,0,1}}));
         trainData.add(DenseMatrix.make(new double[][] {{0,1,0}}));
-       // trainData.add(DenseMatrix.make(new double[][] {{0,1,1}}));
-      //  trainData.add(DenseMatrix.make(new double[][] {{1,0,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,1,1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,0,0}}));
         trainData.add(DenseMatrix.make(new double[][] {{1,0,1}}));
         trainData.add(DenseMatrix.make(new double[][] {{1,1,0}}));
         trainData.add(DenseMatrix.make(new double[][] {{1,1,1}}));
 
         final List<Matrix> teacherSignals = new ArrayList<>();
-        teacherSignals.add(DenseMatrix.make(new double[][] {{1,0}}));
-        teacherSignals.add(DenseMatrix.make(new double[][] {{1,0}}));
-      //  teacherSignals.add(DenseMatrix.make(new double[][] {{0,1}));
-      //  teacherSignals.add(DenseMatrix.make(new double[][] {{1,0}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{0}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1}}));
+
+        final RBM rbm = new RandomRBMFactory().build(3, 1);
+
+        final LearningParameters learningParameters = new LearningParameters().setEpochs(10000).setLearningRate(0.1);
+        final BackErrorPropagation backErrorPropagation = new BackErrorPropagation(learningParameters);
+
+        LOGGER.info(rbm.getWeights());
+        backErrorPropagation.learn(rbm, trainData, teacherSignals);
+        LOGGER.info(rbm.getWeights());
+        for(Matrix input : trainData) {
+            final Matrix output = backErrorPropagation.feedFoward(rbm, input);
+            LOGGER.info(input + " -> " + output.apply(ROUND));
+        }
+    }
+
+    @Test
+    public void backErrorPropagationOrLeftAndRight() {
+        // or
+        final List<Matrix> trainData = new ArrayList<>();
+        trainData.add(DenseMatrix.make(new double[][] {{0,0,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,0,1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,1,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{0,1,1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,0,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,0,1}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,1,0}}));
+        trainData.add(DenseMatrix.make(new double[][] {{1,1,1}}));
+
+        final List<Matrix> teacherSignals = new ArrayList<>();
+        teacherSignals.add(DenseMatrix.make(new double[][] {{0,0}}));
         teacherSignals.add(DenseMatrix.make(new double[][] {{0,1}}));
-        teacherSignals.add(DenseMatrix.make(new double[][] {{0,1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1,1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1,1}}));
         teacherSignals.add(DenseMatrix.make(new double[][] {{1,0}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1,1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1,1}}));
+        teacherSignals.add(DenseMatrix.make(new double[][] {{1,1}}));
 
         final RBM rbm = new RandomRBMFactory().build(3, 2);
 
@@ -59,7 +122,6 @@ public class TestBackErrorPropagation {
             final Matrix output = backErrorPropagation.feedFoward(rbm, input);
             LOGGER.info(input + " -> " + output.apply(ROUND));
         }
-
     }
 
 }
